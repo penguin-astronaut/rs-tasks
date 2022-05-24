3da63e417be70e7223530e5401dfd26a13c8048a
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-25 02:34:25 +0400

    feat: tasksBacklog

---
+++ src/TasksBacklog.ts
@@ -0,0 +1,48 @@
+import { ITask } from './types';
+
+export class TasksBacklog {
+  constructor() {
+    const wrapper = document.querySelector('.wrapper') as HTMLDivElement;
+
+    wrapper.insertAdjacentHTML(
+      'beforeend',
+      `
+     <div class="backlog">
+        <h2 class="backlog__title">Backlog</h2>
+        <input
+          type="text"
+          class="backlog__search"
+          placeholder="Текст или заголовок задачи"
+        />
+        <ul class="backlog-tasks"></ul>
+     </div>
+    `
+    );
+  }
+
+  update(tasks: ITask[]) {
+    const tasksList = document.querySelector(
+      '.backlog-tasks'
+    ) as HTMLUListElement;
+
+    tasks.forEach((task) => {
+      const liElem = document.createElement('li');
+      liElem.classList.add('backlog-tasks__item');
+
+      const taskElem = document.createElement('div');
+      taskElem.classList.add('backlog-task');
+
+      const taskTitleElem = document.createElement('div');
+      taskTitleElem.classList.add('backlog-task__title');
+      taskTitleElem.innerHTML = task.subject;
+
+      const taskDescElem = document.createElement('p');
+      taskDescElem.classList.add('backlog-task__text');
+      taskDescElem.innerHTML = task.description || 'Нет описания';
+
+      taskElem.append(taskTitleElem, taskDescElem);
+      liElem.append(taskElem);
+      tasksList.append(liElem);
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,29 @@ import './assets/style.scss';
 import { TasksApi } from './TasksApi';
 import { getWeekDays } from './datesHelper';
 import { TasksTable } from './TasksTable';
+import { TasksBacklog } from './TasksBacklog';
+import { ITask } from './types';
 
 async function run() {
   const users = await TasksApi.getUsers();
   const tasks = await TasksApi.getTasks();
   const days = getWeekDays();
-  console.log(days);
+
+  const userTasks: ITask[] = [];
+  const backlogTasks: ITask[] = [];
+
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].executor) {
+      userTasks.push(tasks[i]);
+    } else {
+      backlogTasks.push(tasks[i]);
+    }
+  }
+
   const taskTable = new TasksTable();
-  taskTable.updateRows(users, tasks, days);
+  const taskBacklog = new TasksBacklog();
+  taskTable.update(users, userTasks, days);
+  taskBacklog.update(backlogTasks);
 }
 
 run();
